d75a2354e4adc9f55fead54bfe2d22e90d27d253
RMcMullan <root@pve01.internal>
2023-06-14 19:36:09 +0100

    added deployment script and .env file
    Committer: RMcMullan
     On branch dev
    new file:   .env
    new file:   cloud-init-deployments/.env
    new file:   cloud-init-deployments/deploy-pve-vm.sh
    modified:   cloud-init-templates/centos-8-stream.sh

--- cloud-init-templates/centos-8-stream.sh
+++ cloud-init-templates/centos-8-stream.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-# Creats a Centos stream 8 Cloud-Init Ready VM Template in Proxmox
-#
-#
-#
-export IMAGENAME="CentOS-Stream-GenericCloud-8-20230523.0.x86_64.qcow2"
-export IMAGEURL="https://cloud.centos.org/centos/8-stream/x86_64/images/CentOS-Stream-GenericCloud-8-20230523.0.x86_64.qcow2"
-export STORAGE="local-lvm"
-export VMNAME="centos-8-temp"
-export VMID=9000
-export VMMEM=2048
-export VMSETTINGS="--net0 virtio,bridge=vmbr0"
-export CPUTYPE="host"
-export SOCKETS=1
-export CORES=1
+export $(grep -v '^#' .env | xargs) v
+
 
 wget -O ${IMAGENAME} --continue ${IMAGEURL}/${IMAGENAME} && 
 qm create ${VMID} --name ${VMNAME} --memory ${VMMEM} --cores ${CORES} --sockets ${sockets} --cpu ${CPUTYPE} ${VMSETTINGS} && 
