dd3e5d95e16b7c158759a0001f2f5155f5cea93a
RJMcMullan <root@pve01.internal>
2023-06-21 20:41:08 +0100

    added if statement to check if snippes directory exists

--- cloud-init-deployments/deploy-pve-vm.sh
+++ cloud-init-deployments/deploy-pve-vm.sh
@@ -2,35 +2,42 @@
 export $(grep -v '^#' .env | xargs)
 
 # Clone the template into a new VM
-qm clone $TEMPLATE_ID $VMID --name $NAME --full true
+qm clone "$TEMPLATE_ID" "$VMID" --name "$NAME" --full true
 
 # Resize the primary boot disk (otherwise it will be around 10G by default)
-qm resize $VMID scsi0 +$DISKRESIZE
+qm resize "$VMID" scsi0 +"$DISKRESIZE"
 
 # For many Cloud-Init images, it is required to configure a serial console and use it as a display. 
 # If the configuration doesn’t work for a given image however, 
 # switch back to the default display instead.
-qm set $VMID --serial0 socket --vga serial0
+qm set "$VMID" --serial0 socket --vga serial0
 
 # Configure the IP setup. 
-qm set $VMID --ipconfig0 $IPCONFIG0
+qm set "$VMID" --ipconfig0 "$IPCONFIG0"
 
 # Amount of RAM for the VM in MiB.
-qm set $VMID --cores $CORES
+qm set "$VMID" --cores "$CORES"
 # The number of cores per socket.
-qm set $VMID --memory $MEMORY
+qm set "$VMID" --memory "$MEMORY"
 
 # Create a secondary disk for /home directory
-qm set $VMID --scsi1 $STORAGE:$SECONDARY_DISKSIZE
+qm set "$VMID" --scsi1 "$STORAGE":"$SECONDARY_DISKSIZE"
 
+if [ ! -d "$SNIP_DIR" ]
+then
+ 
 # The file needs to be located in a folder that is referred to by "local" storage. 
 # It needs to be in a specific directory structure as shown here:
 # https://pve.proxmox.com/wiki/Storage:_Directory
-# i.e /var/lib/vz/snippets/
-mkdir -p /var/lib/vz/snippets
 
-cp vm-cloud-init.yaml /var/lib/vz/snippets/vm$VMID-cloud-init.yaml
-qm set $VMID --cicustom user=local:snippets/vm$VMID-cloud-init.yaml
+# i.e /var/lib/vz/snippets/
+mkdir -p "$SNIP_DIR"
+fi
+
+cp default-cloud-init.yml /var/lib/vz/snippets/vm-"$VMID"-cloud-init.yml
+qm set "$VMID" --cicustom user=local:snippets/vm-"$VMID"-cloud-init.yaml
 
 # One way to check if the file is run is to start terminal immediately after VM start
-qm start $VMID && qm terminal $VMID
+qm start "$VMID" && qm terminal "$VMID"
+
+echo -e "\e\n[1;32m 🖥 The proxmox vm with the id $VMID has been created! \e[0m"
